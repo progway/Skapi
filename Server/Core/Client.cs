5231acfd9abcafe156e228da6d6d3b33adbddc67
AlexseyChuvilkin <leha.chuvilkin.90@mail.ru>
2018-12-21 14:43:53 +0300

    comit

--- Server/Core/Client.cs
+++ Server/Core/Client.cs
@@ -1,18 +1,23 @@
 ﻿using Noname.Net.RPC;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ServerApp.Core
 {
     public class Client : RPCClientConnectionProvider
     {
+        private bool _isSoundMute;
+
         public Client() { }
 
         public string Nickname;
         public Conference Conference;
-        public bool IsSoundMute;
+        public bool IsSoundMute { get => _isSoundMute; set { _isSoundMute = value;  IsSoundMuteSwitched?.Invoke(this, new IsSoundeMuteEventArgs(value)); } }
+
+        public event EventHandler<IsSoundeMuteEventArgs> IsSoundMuteSwitched;
+    }
+    public class IsSoundeMuteEventArgs : EventArgs
+    {
+        public bool IsSoundMute { get; private set; }
+        public IsSoundeMuteEventArgs(bool isSoundMute) => IsSoundMute = isSoundMute;
     }
 }
--- Server/Core/Conference.cs
+++ Server/Core/Conference.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using NAudio.Midi;
 
 namespace ServerApp.Core
 {
@@ -12,20 +11,48 @@ namespace ServerApp.Core
 
         static Conference() => _idController = new IDController();
 
+        private List<Client> _soundOnUsers;
+
         public Conference(Client creator, IEnumerable<Client> clients, out int id)
         {
             id = _idController.GetID();
             Id = id;
             Creator = new ConferenceUser(creator);
-            Clients = clients.Select(client => new ConferenceUser(client)).ToDictionary(x=> x.Client) ?? throw new ArgumentNullException(nameof(clients));
+            Clients = clients.Select(client => new ConferenceUser(client)).ToDictionary(x => x.Client) ?? throw new ArgumentNullException(nameof(clients));
             foreach (ConferenceUser user in Clients.Values)
                 user.SendRequestToEnterConference(Id, Creator, Clients.Values.ToList());
             Clients.Add(Creator.Client, Creator);
+            _soundOnUsers = new List<Client>();
+            foreach (Client client in Clients.Keys)
+            {
+                client.IsSoundMuteSwitched += Client_IsSoundMuteSwitched;
+                _soundOnUsers.Add(client);
+            }
+        }
+
+        private void Client_IsSoundMuteSwitched(object sender, IsSoundeMuteEventArgs e)
+        {
+            Client client = (Client)sender;
+            if (e.IsSoundMute)
+            {
+                if (_soundOnUsers.Contains(client))
+                    return;
+                _soundOnUsers.Add(client);
+            }
+            else
+            {
+                _soundOnUsers.Remove(client);
+            }
         }
 
         public ConferenceUser Creator;
         public Dictionary<Client, ConferenceUser> Clients { get; private set; }
         public int Id { get; private set; }
+
+        private void SendSound()
+        {
+
+        }
 
         public bool AddClient(Client client)
         {
